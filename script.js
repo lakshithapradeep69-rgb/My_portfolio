6e8626066190b6c2984d3cbe7a955c825e358407
lakshithapradeep69-rgb <lakshithapradeep69@gmail.com>
2026-08-10 22:17:17 +0530

    scriptcreation.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#typing-text", {
+        strings: ["Frontend Developer", "ICT Undergraduate", "UI/UX Designer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
